1f0deab5abe7524e493c18cef6dc3ad349d09277
jhollingworth <jamiehollingworth@gmail.com>
2014-07-29 14:57:51 +0100

    Adding some more git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -13,14 +13,23 @@ alias gd='git diff'
 alias gds='git diff --staged'
 alias gc='git commit -m '
 alias ga="git add -A"
+alias gnb="git checkout -b "
+alias gra="git rebase --abort"
+alias grc="git rebase --continue"
+alias grs="git rebase --skip"
 alias gca='git commit --amend'
 alias gco='git checkout'
 alias gb='git branch -v'
-alias gu='git pull'
+alias gu='git pull --prune'
 alias gp='git push'
+alias gpf='git push -f'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git rebase master"
 alias grh="git reset --hard HEAD"
 alias gr="git remote -v"
 alias gm="git checkout master"
-alias gpt="git push -u origin $(git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+alias g-="git checkout -"
+
+function gt() {
+  git push -u origin $(git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+}
